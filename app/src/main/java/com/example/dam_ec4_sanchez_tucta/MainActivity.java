638e8824b13e99230ad900347bcd9f2cc2a580f3
Andres Sanchez <jugernaru2003@gmail.com>
2024-12-11 17:30:55 -0500

    Notas en columnas

--- app/src/main/java/com/example/dam_ec4_sanchez_tucta/MainActivity.java
+++ app/src/main/java/com/example/dam_ec4_sanchez_tucta/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.dam_ec4_sanchez_tucta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btn1 = (Button) findViewById(R.id.btn_login);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,3 +36,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
---
+++ app/src/main/java/com/example/dam_ec4_sanchez_tucta/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.dam_ec4_sanchez_tucta;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
